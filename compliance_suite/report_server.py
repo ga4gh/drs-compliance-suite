47eaf796419a784453850e1bd38be95cbdbc0ab1
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2023-03-27 15:42:32 -0400

    debug

--- compliance_suite/report_server.py
+++ compliance_suite/report_server.py
@@ -14,33 +14,3 @@ def index():
 def start_mock_server(port):
     # serves the output report as html webpage at http://127.0.0.1:<port>/
     app.run(port=port,debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import http.server
-# import socketserver
-# import os
-# import webbrowser
-# import sys
-#
-# WEB_DIR = os.path.join(os.path.dirname(__file__), 'web2')
-
-
-# def start_mock_server(port):
-#     os.chdir(WEB_DIR)
-#     Handler = http.server.SimpleHTTPRequestHandler
-#     httpd = socketserver.TCPServer(("", port), Handler)
-#     print("serving at http://localhost:" + str(port), file=sys.stderr)
-#     webbrowser.open("http://localhost:" + str(port))
-#     httpd.serve_forever()
