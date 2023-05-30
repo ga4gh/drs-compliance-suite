522e8c185d0903842340a8b4c71f60009a89b2f7
Alex Tsai <alextsaihi@gmail.com>
2023-05-30 11:32:26 -0400

    test_send_request() and other small changes

--- unittests/v1.2.0/test_report_generation.py
+++ unittests/v1.2.0/test_report_generation.py
@@ -1,17 +1,13 @@
 from compliance_suite.report_runner import *
 
 auth_types = ["none", "basic", "bearer", "passport"]
+port_numbers = [8089, 8090, 8091, 8092]
 
-def test_constructor():
-    tvr = ValidateResponse()
-    assert tvr.actual_response == ""
-    assert tvr.expected_response == ""
-    assert tvr.response_schema_file == ""
-    assert tvr.case == ""
-    
+def json_string_formatting(json_string):
+    return str(json_string).replace("'", '"').replace("\\","")
+
 def test_report_runner():
 
-    port_numbers = [8089, 8090, 8091, 8092]
 
     for authtype, port in zip(auth_types, port_numbers):
         good_report = report_runner(server_base_url = "http://localhost:" + str(port) + "/ga4gh/drs/v1",
@@ -34,54 +30,47 @@ def test_report_runner():
                 for case in test["cases"]:
                     case["start_time"] = ""
                     case["end_time"] = ""
-                    assert case["status"] == "PASS"
 
         # USE THIS: uncomment below to generate a new expected_[none/basic/bearer/passport].json
-        # with open('unittests/output/expected_' + authtype + '.json', 'w', encoding="utf-8") as f: json.dump(good_json, f)
+        # with open('unittests/output/expected_good_' + authtype + '.json', 'w', encoding="utf-8") as f: json.dump(good_json, f)
 
         expect_final_json = json.loads(
-            open("unittests/output/expected_" + authtype + ".json", "r").read()
+            open("unittests/output/expected_good_" + authtype + ".json", "r").read()
         )
 
-        actual_json_s = str(good_json).replace("'", '"').replace("\\","")
-        expect_json_s = str(expect_final_json).replace("'", '"').replace("\\","")
+        actual_json_s = json_string_formatting(good_json)
+        expect_json_s = json_string_formatting(expect_final_json)
 
         # expect_final_json will vary throughout production of cs
         assert actual_json_s == expect_json_s
 
 def test_send_request():
-    for auth_type in auth_types:
+    #send_request(server_base_url,endpoint_url,auth_type,auth_token,**kwargs)
+
+    # the other endpoints for possible future tests with drs_object_info and drs_object_access
+    # endpoints = [SERVICE_INFO_URL, DRS_OBJECT_INFO_URL, DRS_ACCESS_URL]
+
+    endpoint_url = SERVICE_INFO_URL
+
+    with open("unittests/resources/request_samples.json", 'r') as file:
+        expected_response = json.load(file)
+
+
+    for authtype, port in zip(auth_types, port_numbers):
+        server_base_url = "http://localhost:"+str(port)+"/ga4gh/drs/v1"
+
+        config_file = "compliance_suite/config/config_samples/config_" + authtype + ".json"
+
+        # the other endpoints for possible future tests with drs_object_info and drs_object_access
+        config_service_info, config_drs_object_info, config_drs_object_access = get_config_json(config_file)
+        auth_token = config_service_info["auth_token"]
+
+        actual_response = send_request(server_base_url, endpoint_url, authtype, auth_token)
+
+        actual_json_s = json_string_formatting(actual_response.text)
+        expect_json_s = json_string_formatting(expected_response)
+
+        assert actual_json_s == expect_json_s
+
+
         
-        auth_token = "user-1"
-
-        request_body = {}
-        if auth_type == "passport":
-            request_body["passports"] = auth_token
-
-        headers = {}
-        if auth_type == "basic":
-            headers = {"Authorization": "Basic {}".format(auth_token)}
-        if auth_type == "bearer":
-            headers = {"Authorization": "Bearer {}".format(auth_token)}
-
-
-        response = send_request("http://localhost:8089/ga4gh/drs/v1",
-                                SERVICE_INFO_URL,
-                                auth_type,
-                                auth_token)
-        
-        
-        actual_response = requests.request(method = "GET",
-                                            url = "http://localhost:8089/ga4gh/drs/v1" + SERVICE_INFO_URL,
-                                            params = {},
-                                            json = {},
-                                            headers = headers)
-
-def test_add_common_test_cases():
-    return
-        
-def test_add_test_case_common():
-    return
-
-def test_add_access_methods_test_case():
-    return
